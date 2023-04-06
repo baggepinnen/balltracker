99086600b9a5163eb740eef173a9530c797a4f65
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-04-06 13:19:19 +0200

    add docs

--- src/balltracker.jl
+++ src/balltracker.jl
@@ -2,6 +2,11 @@ module balltracker
 
 using Images, VideoIO, ImageDraw, ImageView
 
+"""
+    extract_coordinates(img::AbstractMatrix; rad = 8)
+
+Find coordinates of a green ball in an image.
+"""
 function extract_coordinates(img::AbstractMatrix; rad = 8)
     preimg = map(img) do pixel
         hsv = HSV(pixel)
@@ -12,6 +17,23 @@ function extract_coordinates(img::AbstractMatrix; rad = 8)
     ballblob.location
 end
 
+
+"""
+    track_ball(cam; fps = 30, ballradius = 8, markerradius = 8, markercolor = RGB(1, 0, 0), duration = 5, callback = display, downsampling = 2, sleep = sleep)
+
+Track a ball in a video stream.
+
+# Arguments:
+- `cam`: An object that iterates images when calling `read(cam)`, e.g., `VideoIO.openvideo(path)` or `VideoIO.opencamera()`.
+- `fps`: Frames per second to track the ball at.
+- `ballradius`: Radius of blobs to detect. A larger radius is more computaionally expensive.
+- `markerradius`: To indicate the found ball
+- `markercolor`: To indicate the found ball
+- `duration`: Maximum duration to track the ball for (seconds).
+- `callback`: a function taking an image and does something interesting.
+- `downsampling`: An integer specifying a downsampling factor for the image before blob detection.
+- `sleep`: The function used to sleep, alternatives include `Libc.systemsleep`
+"""
 function track_ball(cam;
         fps = 30,
         ballradius = 8,
@@ -20,6 +42,7 @@ function track_ball(cam;
         duration = 5,
         callback = display,
         downsampling = 2,
+        sleep = sleep,
     )
 
     errs = 0
@@ -37,12 +60,16 @@ function track_ball(cam;
             errs += 1
             @error e
         end
-        # Libc.systemsleep(max(1e-3, 1/fps-execution_time))
-        sleep(max(1/200, 1/fps-execution_time))
+        sleep(max(0, 1/fps-execution_time))
         i += 1
     end
 end
 
+"""
+    julia_main()
+
+Launches [`track_ball`](@ref) with ImageView as display.
+"""
 function julia_main()::Cint
     isempty(ARGS) && error("No arguments given, please provide the path to an interesting video file")
     path = ARGS[1]
